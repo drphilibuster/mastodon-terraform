89e90c0e22214f58df2d7359a2584698b18bd0b8
Jim Frontz <jim.frontz@rackspace.com>
2022-11-25 21:20:33 -0600

    added variables for aws creds

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region                = "us-east-1"
-  AWS_ACCESS_KEY_ID     = ""
-  AWS_SECRET_ACCESS_KEY = ""
+  AWS_ACCESS_KEY_ID     = var.mastodon_aws_access_key_id
+  AWS_SECRET_ACCESS_KEY = var.mastodon_aws_secret_access_key
 }

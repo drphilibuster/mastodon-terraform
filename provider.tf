4e2d5baec48f6f0790871b6475033a254dd5693b
Jim Frontz <jim.frontz@rackspace.com>
2022-11-25 21:23:25 -0600

    updates provider format

--- provider.tf
+++ provider.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4"
-    }
-  }
-}
-
-# Configure the AWS Provider
 provider "aws" {
-  region                = "us-east-1"
-  AWS_ACCESS_KEY_ID     = var.mastodon_aws_access_key_id
-  AWS_SECRET_ACCESS_KEY = var.mastodon_aws_secret_access_key
+  version = "~> 4.0"
+  region  = "us-east-1"
 }

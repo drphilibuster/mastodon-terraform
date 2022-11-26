3eb0bba15c0f8303d8e58f3ce25c42ad28516797
Jim Frontz <jim.frontz@rackspace.com>
2022-11-25 20:29:19 -0600

    ads cors rule resource

--- modules/mastodon/aws_s3_bucket.tf
+++ modules/mastodon/aws_s3_bucket.tf
@@ -1,10 +1,14 @@
 resource "aws_s3_bucket" "mastodon" {
   bucket = var.aws_s3_bucket_name
+}
+
+resource "aws_s3_bucket_cors_configuration" "mastadon_cors" {
+  bucket = aws_s3_bucket.mastadon.id
 
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "HEAD"]
     allowed_origins = ["*"]
-    max_age_seconds = "3000"
+    max_age_seconds = 3000
   }
 }

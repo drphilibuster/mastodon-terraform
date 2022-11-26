e1a605c317fbd557709a18de3ba366ca4bc1fa00
Jim Frontz <jim.frontz@rackspace.com>
2022-11-25 20:53:21 -0600

    corrects cache behavior

--- modules/mastodon/aws_cloudfront_distribution.tf
+++ modules/mastodon/aws_cloudfront_distribution.tf
@@ -2,7 +2,24 @@ resource "aws_cloudfront_distribution" "mastodon" {
   aliases = ["${var.mastodon_local_domain}"]
   comment = "mastodon"
   enabled = true
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    default_ttl            = 0
+    max_ttl                = 0
+    min_ttl                = 0
+    viewer_protocol_policy = var.aws_acm_certificate_arn == "" ? "allow-all" : "redirect-to-https"
+    target_origin_id       = "mastodon_alb"
 
+    forwarded_values {
+      headers      = ["*"]
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+  }
   ordered_cache_behavior {
     path_pattern           = "/assets/*"
     allowed_methods        = ["GET", "HEAD"]
@@ -28,25 +45,6 @@ resource "aws_cloudfront_distribution" "mastodon" {
   custom_error_response {
     error_caching_min_ttl = 0
     error_code            = 404
-  }
-
-  ordered_cache_behavior {
-    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
-    cached_methods         = ["GET", "HEAD"]
-    default_ttl            = 0
-    max_ttl                = 0
-    min_ttl                = 0
-    viewer_protocol_policy = var.aws_acm_certificate_arn == "" ? "allow-all" : "redirect-to-https"
-    target_origin_id       = "mastodon_alb"
-
-    forwarded_values {
-      headers      = ["*"]
-      query_string = true
-
-      cookies {
-        forward = "all"
-      }
-    }
   }
 
   origin {
